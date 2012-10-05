cf80501046c33f7d5771acfb92b0fa38bd12308b
Marcelo J. Both <marcelo.both@gmail.com>
2012-10-05 00:20:25 -0300

    Settings para o heroku

--- mycms/src/settings.py
+++ mycms/src/settings.py
@@ -13,7 +13,9 @@ ADMINS = (
 MANAGERS = ADMINS
 
 
-DATABASES = {}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://marcelo.both@gmail.com:141105@virtual-cms.herokuapp.com/:5432/cms_db')}
+
 
 #DATABASES = {
 #    'default': {
