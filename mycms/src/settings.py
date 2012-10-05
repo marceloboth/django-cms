650be1bd264b2500ba89e518cfe94638eabf954d
Marcelo J. Both <marcelo.both@gmail.com>
2012-10-05 00:01:43 -0300

    Settings para o heroku

--- mycms/src/settings.py
+++ mycms/src/settings.py
@@ -12,7 +12,15 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(PROJECT_PATH, 'database.sqlite'),
+#    }
+#}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
