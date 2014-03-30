6c55d9be53188e77333755b14f44e4a7daf0be9d
Marcelo J. Both <marcelo.both@gmail.com>
2014-03-30 10:07:26 -0300

    Alteração da base de dados

--- mycms/src/settings.py
+++ mycms/src/settings.py
@@ -13,16 +13,12 @@ ADMINS = (
 MANAGERS = ADMINS
 
 
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_BROWN_URL"])}
-
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(PROJECT_PATH, 'database.sqlite'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_PATH, 'database.sqlite'),
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
